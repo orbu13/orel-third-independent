3754be1e2761b8efc455ebcc029f4aef47f50fc0
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-09 18:56:46 -0700

    working on my JS

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+let form = document.getElementById("numberForm")
+
+form.addEventListener("submit", e => {
+  e.preventDefault()
+
+  let inputValue = document.getElementById("numberInput").value
+})
