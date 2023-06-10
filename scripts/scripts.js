a69d36c3d8bc90e8a46d88d59979e7bb22cc450f
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-09 20:50:39 -0700

    working on my JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,30 +1,7 @@
 let form = document.getElementById("numberForm")
 
-form.addEventListener("submit", e => {
-  e.preventDefault()//prevent refresh
+form.addEventListener() // prevent refresh
 
-  // getting value of input
-  let inputValue = document.getElementById("numberInput").value
-  // get hold of output div
-  let outputDiv = document.getElementById("output")
-  let result = []
+let inpuValue = document.getElementById("numberInput").value
 
-  // looping through to inputvalue and pushing as string to numbers
-  for (let i = 0; i <= inputValue; i++) {
-    let num = `${i}`
-    if (num.includes("3")) {
-      result.push("Won't you be my neighbor?!")
-    }
-    else if (num.includes("2")) {
-      result.push("Boop!")
-    }
-    else if (num.includes("1")) {
-      result.push("Beep!")
-    }
-    else {
-      result.push(num)
-    }
-  }
-
-  outputDiv.innerHTML = result.join(", ")
-})
+let inpuValue = document.getElementById("numberInput").value
