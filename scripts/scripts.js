baf8155aa1a5a4bf057d24590631c76df1f68de6
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-11 18:50:34 -0700

    working on my JS code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,16 @@
+let form = document.getElementById("numberForm")
+
+form.addEventListener("submit", e => {
+  e.preventDefault() //prevent refresh
+
+  let inputValue = document.getElementById("numberInput").value
+
+  let outputDive.innerHTML = beepBoop(inputValue)
+
+})
+
+
+function beepBoop(inputValue) {
+  let result = []
+}
 
