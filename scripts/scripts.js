b26896e662b3521705c596f569f5e72d08b47721
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-11 18:09:34 -0700

    working on my project

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,1 @@
-let form = document.getElementById("numberForm")
 
-form.addEventListener() // prevent refresh
-
-let inpuValue = document.getElementById("numberInput").value
-
-let inpuValue = document.getElementById("numberInput").value
