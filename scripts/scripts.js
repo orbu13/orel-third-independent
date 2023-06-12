4808ac868af17bff06a5481894c703475649bed0
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-11 20:01:54 -0700

    updating JV code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,8 +9,18 @@ form.addEventListener("submit", e => {
 
 })
 
-
 function beepBoop(inputValue) {
   let result = []
-}
 
+  for (let i = 0; i <= inputValue; i++) {
+    let num = i + ""
+
+    if (num.includes("1")) {
+      result.push("Won't you be my neighbor?!")
+    }
+    else if (num.includes("2")) {
+      result.push("Boop!")
+    }
+    else if (num.includes("3")) {
+      result.push("Beep!")
+    }
