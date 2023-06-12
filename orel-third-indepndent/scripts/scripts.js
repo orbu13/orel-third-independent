8953f1d4d7d89cd505282b5387bfc603c05057c1
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-11 22:06:08 -0700

    JS is complied

---
+++ orel-third-indepndent/scripts/scripts.js
@@ -0,0 +1,33 @@
+let form = document.getElementById("numberForm")
+
+form.addEventListener("submit", e => {
+  e.preventDefault() //prevent refresh
+
+  let inputValue = document.getElementById("numberInput").value
+
+  let outputDive.innerHTML = beepBoop(inputValue)
+
+})
+
+function beepBoop(inputValue) {
+  let result = []
+
+  for (let i = 0; i <= inputValue; i++) {
+    let num = i + ""
+
+    if (num.includes("3")) {
+      result.push("Won't you be my neighbor?!")
+    }
+    else if (num.includes("2")) {
+      result.push("Boop!")
+    }
+    else if (num.includes("1")) {
+      result.push("Beep!")
+    }
+    else {
+      result.push(num)
+    }
+  }
+
+  return result.join(", ")
+}
