072622f12e8a3b141783b8ca3d18796a43f6ed80
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-18 12:47:27 -0700

    submitting file

--- orel-third-indepndent/scripts/scripts.js
+++ orel-third-indepndent/scripts/scripts.js
@@ -1,30 +1,47 @@
-let form = document.getElementById("numberForm")
+// User Interface Logic
+
+// Function to handle form submission
+function handleSubmit(event) {
+  event.preventDefault(); // prevent refresh
+
+  // Get input value
+  let inputValue = document.getElementById("numberInput").value;
+
+  // Call business logic function and display the result
+  let result = processNumbers(inputValue);
+  displayResult(result);
+}
+
+// Function to display the result in the output element
+function displayResult(result) {
+  let outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = result.join(", ");
+}
+
+// Add event listener to the form submit event
+let form = document.getElementById("numberForm");
+form.addEventListener("submit", handleSubmit);
 
 
-form.addEventListener("submit", e => {
-  e.preventDefault()//prevent refresh
 
-  let inputValue = document.getElementById("numberInput").value
+// Business Logic
 
-  let outputDiv = document.getElementById("output")
-  let result = []
+// Function to process numbers
+function processNumbers(inputValue) {
+  let result = [];
 
   for (let i = 0; i <= inputValue; i++) {
-    let num = i + ""
+    let num = i + "";
     if (num.includes("3")) {
-      result.push("Won't you be my neighbor?!")
-    }
-    else if (num.includes("2")) {
-      result.push("Boop!")
-    }
-    else if (num.includes("1")) {
-      result.push("Beep!")
-    }
-    else {
-      result.push(num)
+      result.push("Won't you be my neighbor?!");
+    } else if (num.includes("2")) {
+      result.push("Boop!");
+    } else if (num.includes("1")) {
+      result.push("Beep!");
+    } else {
+      result.push(num);
     }
   }
 
-
-  outputDiv.innerHTML = result.join(", ")
-})
+  return result;
+}
